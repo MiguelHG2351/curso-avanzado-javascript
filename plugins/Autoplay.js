904487a95bdde9fb532169e59d1f7809568b6715
Miguel Hernández Gaitan <60478285+MiguelHG2351@users.noreply.github.com>
2020-09-23 09:35:42 -0600

    generators and proxys

--- plugins/Autoplay.js
+++ plugins/Autoplay.js
@@ -3,6 +3,8 @@
 export default function AutoPlay() {}
 
 AutoPlay.prototype.run = function (player) {
-    player.mute()
+    if(!player.muted) {
+        player.muted = true
+    }
     player.play()
 }
